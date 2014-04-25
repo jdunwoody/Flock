1a49ded069d9bbb9a800e4df54de5feaef2f4956
James Dunwoody <jamesd@hooroo.com>
2014-04-26 07:50:25 +1000

    Removing bunny test stuff

--- javascript/js/vehicle.js
+++ javascript/js/vehicle.js
@@ -1,21 +1,3 @@
-function buildBunny(width, height) {
-  var texture = new PIXI.Texture.fromImage("img/bunny.png");
-  var bunny = new PIXI.Sprite(texture);
-
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  bunny.position.x = width/2;
-  bunny.position.y = height/2;
-
-  return bunny;
-};
-
-function moveBunny(bunny) {
-  bunny.rotation += 0.01;
-};
-
-
 function Heading() {
   this.value = vec2.create();
 };
