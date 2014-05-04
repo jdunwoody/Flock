418b90bfc6cb270b9edb38836511f0bf50175ddf
James Dunwoody <jamesd@hooroo.com>
2014-05-04 22:24:07 +1000

    Up to Building a Larger Map

--- javascript/js/Walls.js
+++ javascript/js/Walls.js
@@ -18,14 +18,13 @@ Walls.VIEWPORT_WIDTH = 512;
 Walls.VIEWPORT_NUM_SLICES = Math.ceil(Walls.VIEWPORT_WIDTH/WallSlice.WIDTH) + 1;
 
 Walls.prototype.setViewportX = function(viewportX) {
-  var maxViewportX = (this.slices.length - Walls.VIEWPORT_NUM_SLICES) *
-    WallSlice.WIDTH;
-  if (viewportX < 0) {
-    viewportX = 0;
-  } else if (viewportX >= maxViewportX) {
-    viewportX = maxViewportX;
-  }
-  this.viewportX = viewportX;
+  this.viewportX = this.checkViewportXBounds(viewportX);
+
+  var prevViewportSliceX = this.viewportSliceX;
+  this.viewportSliceX = Math.floor(this.viewportX/WallSlice.WIDTH);
+
+  this.removeOldSlices(prevViewportSliceX);
+  this.addNewSlices();
 };
 
 Walls.prototype.addSlice = function(sliceType, y) {
@@ -43,6 +42,43 @@ Walls.prototype.checkViewportXBounds = function(viewportX) {
   }
 
   return viewportX;
+};
+
+Walls.prototype.removeOldSlices = function(prevViewportSliceX) {
+  var numOldSlices = this.viewportSliceX - prevViewportSliceX;
+  if (numOldSlices > Walls.VIEWPORT_NUM_SLICES) {
+    numOldSlices = Walls.VIEWPORT_NUM_SLICES;
+  }
+
+  for (var i = prevViewportSliceX; i < prevViewportSliceX + numOldSlices; i++) {
+    var slice = this.slices[i];
+    if (slice.sprite != null) {
+      this.returnWallSprite(slice.type, slice.sprite);
+      this.removeChild(slice.sprite);
+      slice.sprite = null;
+    }
+  }
+};
+
+Walls.prototype.addNewSlices = function() {
+  var firstX = -(this.viewportX % WallSlice.WIDTH);
+  for (var i = this.viewportSliceX, sliceIndex = 0;
+      i < this.viewportSliceX + Walls.VIEWPORT_NUM_SLICES;
+      i++, sliceIndex++) {
+        var slice = this.slices[i];
+        if (slice.sprite == null && slice.type != SliceType.GAP) {
+          // Associate the slice with a sprite and update the sprite's position
+          slice.sprite = this.borrowWallSprite(slice.type);
+
+          slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+          slice.sprite.position.y = slice.y;
+
+          this.addChild(slice.sprite);
+        } else if (slice.sprite != null) {
+          // The slice is already associated with a sprite. Just update its position
+          slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+        }
+      }
 };
 
 Walls.prototype.createLookupTables = function() {
